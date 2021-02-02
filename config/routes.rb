f2d3ce1e418018b010ae673cc89c8a4d7472340c
Joseph Budina <joebud3@gmail.com>
2021-02-02 13:58:34 -0700

    Fix file path names to welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'ice_cream_parlor#index'
+  get '/', to: 'welcome#index'
+  get '/ice_cream_parlor', to: 'ice_cream_parlor#index'
+  get '/coffee_company', to: 'coffee_company#index'
+  get '/ice_cream_parlor', to: 'ice_cream_parlor#new'
+  get '/coffee_company', to: 'coffee_company#index'
 end
