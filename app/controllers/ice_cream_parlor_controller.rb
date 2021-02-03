5fd7828aff048e463b6a74b6f1c70407f7890b5e
Joseph Budina <joebud3@gmail.com>
2021-02-03 16:44:57 -0700

    Add edit method

--- app/controllers/ice_cream_parlor_controller.rb
+++ app/controllers/ice_cream_parlor_controller.rb
@@ -5,6 +5,10 @@ class IceCreamParlorController < ApplicationController
 
   def new
   end
+  
+  def show
+    @parlors = IceCreamParlor.find(params[:id])
+  end
 
   def create
     parlor = IceCreamParlor.new({
@@ -17,7 +21,16 @@ class IceCreamParlorController < ApplicationController
     redirect_to '/ice_cream_parlor'
   end
 
-  def show
+  def edit
     @parlors = IceCreamParlor.find(params[:id])
+  end
+
+  def update
+    parlor = IceCreamParlor.find(params[:id])
+    parlor.update({
+      name: params[:ice_cream_parlor][:name],
+      open: params[:ice_cream_parlor][:open],
+      street_number: params[:ice_cream_parlor][:street_number]
+    })
   end
 end
