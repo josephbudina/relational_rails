315e7147963d427f379995c6412c21186a85b586
Jenny Branham <branhamjenny@gmail.com>
2021-02-04 11:09:36 -0600

    Add coffee roasts

--- app/controllers/coffee_roast_controller.rb
+++ app/controllers/coffee_roast_controller.rb
@@ -10,4 +10,45 @@ class CoffeeRoastController < ApplicationController
   def coffee_company_roasts
     @company = CoffeeCompany.find(params[:id])
   end
+
+  def create
+    if params[:roast][:fresh].nil?
+      params[:roast][:fresh] = false
+    end
+    roast = CoffeeRoast.create({
+      name: params[:roast][:name],
+      coffee_company_id: params[:company],
+      origin: params[:roast][:origin],
+      elevation: params[:roast][:elevation],
+      fresh: params[:roast][:fresh],
+      })
+    redirect_to "/coffee_roast"
+  end
+
+  def edit
+    @roast = CoffeeRoast.find(params[:id])
+  end
+
+  def update
+    if params[:roast][:fresh].nil?
+      params[:roast][:fresh] = false
+    end
+    roast = CoffeeRoast.find(params[:id])
+
+    roast.update({
+      name: params[:roast][:name],
+      coffee_company_id: params[:company],
+      origin: params[:roast][:origin],
+      elevation: params[:roast][:elevation],
+      fresh: params[:roast][:fresh]
+      })
+    roast.save
+
+    redirect_to "/coffee_roast/#{roast.id}"
+  end
+
+  def destroy
+    CoffeeRoast.destroy(params[:id])
+    redirect_to "/coffee_company"
+  end
 end
