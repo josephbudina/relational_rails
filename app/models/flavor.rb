15d17f617a21c94c5dcb1c831bfb5c1a6ec5a272
Joseph Budina <joebud3@gmail.com>
2021-02-04 18:03:34 -0700

    Add flavor test and update attributes

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -1,2 +1,7 @@
 class Flavor < ApplicationRecord
+  validates_presence_of   :flavor_name,
+                          :in_stock,
+                          :flavor_rating
+  validates_uniqueness_of :name
+  belongs_to  :ice_cream_parlor, class_name: "IceCreamParlor"
 end
---
+++ spec/models/flavor_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Flavor, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :flavor_name}
+    it {should validate_presence_of :in_stock}
+    it {should validate_presence_of :flavor_rating}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :ice_cream_parlor}
+  end
+end
