f98468e70dbaac7cc24adf870ace6a31603347fb
Joseph Budina <joebud3@gmail.com>
2021-02-07 11:39:41 -0700

    Add flavor model

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -1,6 +1,5 @@
 class Flavor < ApplicationRecord
-  validates_presence_of   :flavor_name,
-                          :in_stock,
+  validates_presence_of   :name,
                           :flavor_rating
   validates_uniqueness_of :name
   belongs_to  :ice_cream_parlor, class_name: "IceCreamParlor"
