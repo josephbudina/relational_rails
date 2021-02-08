01d77cdb214c3a0e609f3b7f4a627e319a57315a
Jenny Branham <branhamjenny@gmail.com>
2021-02-08 16:51:39 -0600

    Fix coffee company index spec

--- spec/features/coffee_companies/index_spec.rb
+++ spec/features/coffee_companies/index_spec.rb
@@ -1,28 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'The coffee company index page', type: :feature do
-  it "can see each coffee company" do
-    company_1 = CoffeeCompany.create(name:"Round Mountain Coffee",
-                                     address: "123 Prince St",
-                                     zipcode: 72034,
-                                     local: true)
-    company_2 = CoffeeCompany.create(name:"Onyx Coffee Labs",
-                                     address: "123 W St",
-                                     zipcode: 72712,
-                                     local: false)
+RSpec.describe 'Coffee company index page', type: :feature do
+  describe "as a visitor" do
+    describe "when I visit /coffee_companies" do
+      it "I can see each coffee company" do
+        company_1 = CoffeeCompany.create(name:"Round Mountain Coffee",
+                                        address: "123 Prince St",
+                                        zipcode: 72034,
+                                        local: true)
+        company_2 = CoffeeCompany.create(name:"Onyx Coffee Labs",
+                                        address: "123 W St",
+                                        zipcode: 72712,
+                                        local: false)
 
-    visit '/coffee_companies'
-    expect(page).to have_content('Coffee Companies')
-    expect(page).to have_content(company_1.name)
-    expect(page).to have_content(company_2.name)
+        visit '/coffee_companies'
+        expect(page).to have_content('Coffee Companies')
+        expect(page).to have_link(company_1.name)
+        expect(page).to have_link(company_2.name)
 
-    expect(page).to have_link('New Coffee Company')
-  end
+        expect(page).to have_link('New Coffee Company')
+      end
 
-  it "can see no coffee companies if there are none" do
-    visit '/coffee_companies'
-    expect(page).to have_content('Coffee Companies')
-    expect(page).to have_content('No Coffee Companies!')
-    expect(page).to have_link('New Coffee Company')
+      it "can see no coffee companies if there are none" do
+        visit '/coffee_companies'
+        expect(page).to have_content('Coffee Companies')
+        expect(page).to have_content('No Coffee Companies!')
+        expect(page).to have_link('New Coffee Company')
+      end
+    end
   end
 end
