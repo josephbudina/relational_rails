83b9273fc85a73cdc90747df7fbb92a50a991101
Jenny Branham <branhamjenny@gmail.com>
2021-02-08 17:00:02 -0600

    Fix company show spec

--- spec/features/coffee_companies/show_spec.rb
+++ spec/features/coffee_companies/show_spec.rb
@@ -1,39 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'The coffee company show page', type: :feature do
-  it "can see the coffee company and its attributes" do
+  describe "as a visitor" do
+    describe "when I visit /coffee_companies/:id" do
+      it "I can see the coffee company and its attributes" do
+        company_1 = CoffeeCompany.create(name:"Round Mountain Coffee",
+                                        address: "123 Prince St",
+                                        zipcode: 72034,
+                                        local: true)
+        company_2 = CoffeeCompany.create(name:"Onyx Coffee Labs",
+                                        address: "123 W St",
+                                        zipcode: 72712,
+                                        local: false)
 
-    company_1 = CoffeeCompany.create(name:"Round Mountain Coffee",
-                                     address: "123 Prince St",
-                                     zipcode: 72034,
-                                     local: true)
-    company_2 = CoffeeCompany.create(name:"Onyx Coffee Labs",
-                                     address: "123 W St",
-                                     zipcode: 72712,
-                                     local: false)
+        visit '/coffee_companies'
+        click_link("Round Mountain Coffee")
 
-    visit '/coffee_companies'
-    click_link("Round Mountain Coffee")
+        expect(current_path).to eq ("/coffee_companies/#{company_1.id}")
 
-    expect(current_path).to eq ("/coffee_companies/#{company_1.id}")
+        expect(page).to have_content('Round Mountain Coffee')
+        expect(page).to have_content("Address: #{company_1.address}")
+        expect(page).to_not have_content(company_2.name)
+        expect(page).to have_content("Local?")
+      end
 
-    expect(page).to have_link('Coffee Company Index')
-    expect(page).to have_content('Round Mountain Coffee')
-
-    expect(page).to have_link('View Roasts')
-    expect(page).to have_content("Address: #{company_1.address}")
-    expect(page).to_not have_content(company_2.name)
-    expect(page).to have_content("Local?")
-
-    expect(page).to have_link('Update Company')
-    expect(page).to have_button('Delete Company')
+      it "can see no coffee companies if there are none" do
+        visit '/coffee_companies'
+        expect(page).to have_content('Coffee Companies')
+        expect(page).to have_content('No Coffee Companies!')
+      end
+    end
   end
-
-  it "can see no coffee companies if there are none" do
-    visit '/coffee_companies'
-    expect(page).to have_content('Coffee Companies')
-    expect(page).to have_content('No Coffee Companies!')
-    expect(page).to have_link('New Coffee Company')
-  end
-
 end
